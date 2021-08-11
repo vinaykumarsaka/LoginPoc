8ae51b6f0f14ee0a9c210750f9bc66b5caa5ced0
akhilnv12 <akhil.nemana@gmail.com>
2021-08-11 12:58:03 +0530

    Added navbar and login pages

--- WebApp/CLientApp/src/app/app.module.ts
+++ WebApp/CLientApp/src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< Updated upstream
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderInterceptor } from './interceptors/header.interceptor';
+=======
+import { HeaderComponent } from './layout/header/header.component';
+>>>>>>> Stashed changes
 
 
 @NgModule({
   declarations: [
+<<<<<<< Updated upstream
     AppComponent
+=======
+    AppComponent,
+    HeaderComponent
+  
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true },
--- WebApp/CLientApp/src/app/modules/auth/auth.module.ts
+++ WebApp/CLientApp/src/app/modules/auth/auth.module.ts
@@ -9,12 +9,18 @@ import { MatInputModule } from '@angular/material/input';
 import {ReactiveFormsModule,FormsModule} from '@angular/forms'
 import { SigninComponent } from './signin/signin.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [SigninComponent],
+  declarations: [SigninComponent, SignupComponent],
   imports: [
     MatButtonModule,
     MatCheckboxModule,
@@ -26,7 +32,12 @@ import { AuthRoutingModule } from './auth-routing.module';
     CommonModule,
     RouterModule,
     MatProgressSpinnerModule,
-    AuthRoutingModule
+    MatCardModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    AuthRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class AuthModule { }
