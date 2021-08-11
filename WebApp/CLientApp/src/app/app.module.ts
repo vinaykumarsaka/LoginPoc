52dc93fdc18dc29ef3edd43e4f900fa4e26019e9
nimishavarma055 <nbrahmarouthu@venhan.com>
2021-08-11 12:54:47 +0530

    Added header interceptor

--- WebApp/CLientApp/src/app/app.module.ts
+++ WebApp/CLientApp/src/app/app.module.ts
@@ -4,19 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeaderInterceptor } from './interceptors/header.interceptor';
 
 
 @NgModule({
   declarations: [
     AppComponent
-  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ WebApp/CLientApp/src/app/interceptors/header.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HeaderInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const authReq = request.clone({
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token'
+      })
+    });
+
+  //  Adding extra attributes
+  //   const authReq = request.clone({
+  //     headers: request.headers.set('Content-Type', 'application/json')
+  //     .set('header2', 'header 2 value')
+  //     .set('header3', 'header 3 value')
+  // });
+    return next.handle(authReq);
+  }
+}
